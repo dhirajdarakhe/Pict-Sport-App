69a0049920993eb7890181bbd65b807077c33f3a
Harshwardhan431 <81025973+Harshwardhan431@users.noreply.github.com>
2022-02-05 01:29:25 +0530

    tt firebase connected

--- lib/screens/calender/table_tennis/table_tannis_main_screen.dart
+++ lib/screens/calender/table_tennis/table_tannis_main_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:psa/models/userDetails.dart';
+import 'package:psa/screens/calender/table_tennis/requests.dart';
 import 'package:psa/screens/calender/table_tennis/table_status_popup.dart';
 import 'package:psa/screens/calender/table_tennis/table_tennis_issue_screen.dart';
 
@@ -10,14 +13,52 @@ class TabletannisScreen extends StatefulWidget {
 }
 
 class _TabletannisScreenState extends State<TabletannisScreen> {
-  var isRacketIssued = false;
-  // const Demo({Key? key}) : super(key: key);
+
+  late bool _isRequested=true;
+  bool _isFirstVisit=false;
+
+  Future getStatus()async{
+    var v=await FirebaseFirestore.instance.collection('EquipmentIssuing')
+        .doc('TT').collection('Equipment')
+        .doc(UserDetails.uid).get();
+
+    if (v.exists){
+      _isRequested=v.get('isRequested');
+      setState(() {
+        _isFirstVisit=false;
+      });
+      print('RRR');
+      }
+    else{
+      print('FFFF');
+     // _isRequested=false;
+      setState(() {
+        _isFirstVisit=true;
+      });
+    }
+
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    getStatus();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text(""
-              "Table_Page"),
+              "Table Tennis",),
+          actions: [
+            IconButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context){
+                return Request();
+              }));
+            },
+                icon: const Icon(Icons.request_page_outlined,size: 30,))
+          ],
         ),
         body: Padding(
           padding:
@@ -28,132 +69,15 @@ class _TabletannisScreenState extends State<TabletannisScreen> {
               const SizedBox(
                 height: 10,
               ),
-              Expanded(
-                flex: 3,
-                child: GestureDetector(
-                  onTap: () {
-                    showModalBottomSheet<void>(
-                        backgroundColor: Colors.lightBlueAccent,
-                        shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                                // bottomRight: Radius.circular(50.0),
-                                topRight: Radius.circular(50.0),
-                                topLeft: Radius.circular(56.0))),
-                        context: context,
-                        builder: (context) {
-                          return TableStatusPopup(
-                            tableno: 'Table NO 01',
-                            unEnrollSeats: 3,
-                          );
-                        });
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.only(right: 10),
-                    // width: double.infinity,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      image: const DecorationImage(
-                        image: AssetImage("assets/table_tenis_1.jpg"),
-                        // fit: BoxFit.fitWidth,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    child: const Text(
-                      "TABLE NO 01",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                      // Image.asset("assets/images/table_tenis_1.jpg",fit: BoxFit.fitHeight,
-                    ),
-                  ),
-                ),
-              ),
+              TTWidget(tableNumber: '01',enrolledSeats: 3,image: "assets/table_tenis_1.jpg",),
               const SizedBox(
                 height: 15,
               ),
-              Expanded(
-                flex: 3,
-                child: GestureDetector(
-                  onTap: () {
-                    showModalBottomSheet<void>(
-                        backgroundColor: Colors.lightBlueAccent,
-                        shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                                // bottomRight: Radius.circular(50.0),
-                                topRight: Radius.circular(50.0),
-                                topLeft: Radius.circular(56.0))),
-                        context: context,
-                        builder: (context) {
-                          return TableStatusPopup(
-                            tableno: 'Table NO 02',
-                            unEnrollSeats: 4,
-                          );
-                        });
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.only(right: 10),
-                    // width: double.infinity,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      image: const DecorationImage(
-                        image: AssetImage("assets/table_tenis_2.jpg"),
-                        // fit: BoxFit.fitWidth,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    child: const Text(
-                      "TABLE NO 02",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                      // Image.asset("assets/images/table_tenis_1.jpg",fit: BoxFit.fitHeight,
-                    ),
-                  ),
-                ),
-              ),
+              TTWidget(image: "assets/table_tenis_2.jpg", enrolledSeats: 2, tableNumber: '02'),
               const SizedBox(
                 height: 15,
               ),
-              Expanded(
-                flex: 3,
-                child: GestureDetector(
-                  onTap: () {
-                    showModalBottomSheet<void>(
-                        backgroundColor: Colors.lightBlueAccent,
-                        shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                                // bottomRight: Radius.circular(50.0),
-                                topRight: Radius.circular(50.0),
-                                topLeft: Radius.circular(56.0))),
-                        context: context,
-                        builder: (context) {
-                          return TableStatusPopup(
-                            tableno: 'Table NO 03',
-                            unEnrollSeats: 1,
-                          );
-                        });
-                  },
-                  child: Container(
-                    padding: EdgeInsets.only(right: 10),
-                    // width: double.infinity,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      image: const DecorationImage(
-                        image: AssetImage("assets/table_tenis_1.jpg"),
-                        // fit: BoxFit.fitWidth,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    child: const Text(
-                      "TABLE NO 03",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                      // Image.asset("assets/images/table_tenis_1.jpg",fit: BoxFit.fitHeight,
-                    ),
-                  ),
-                ),
-              ),
+              TTWidget(image: "assets/table_tenis_1.jpg",enrolledSeats: 2,tableNumber: '03',),
               const SizedBox(
                 height: 12,
               ),
@@ -164,26 +88,23 @@ class _TabletannisScreenState extends State<TabletannisScreen> {
                   // margin: const EdgeInsets.all(12.0),
                   child: RaisedButton(
                     splashColor: Colors.lightBlueAccent,
-                    elevation: 10.0, shape: StadiumBorder(),
-                    child: isRacketIssued
-                        ? const Text(
-                            'Issue The Racket',
+                    elevation: 10.0, shape: const StadiumBorder(),
+                    child:_isFirstVisit? const Text(
+                            'Issue the Racked',
                             style: TextStyle(fontSize: 16),
-                          )
-                        : const Text(
-                            'Issue The Racket',
-                            style: const TextStyle(fontSize: 16),
-                          ),
-                    //makes background color white
+                          ):_isRequested? const Text(
+                            'Requested',
+                            style: TextStyle(fontSize: 16),
+                          ):const Text(
+                      'Return the Racket',
+                      style: TextStyle(fontSize: 16),
+                    ),
                     onPressed: () {
-                      setState(() {
-                        isRacketIssued = true;
-                      });
-                      Navigator.push(context,
+                      /*Navigator.push(context,
                           MaterialPageRoute(builder: (context) {
                         return IssueTheRacket();
-                      }));
-                      print("FlatButton tapped");
+                      }));*/
+
                     },
                   ),
                 ),
@@ -191,5 +112,59 @@ class _TabletannisScreenState extends State<TabletannisScreen> {
             ],
           ),
         ));
+  }
+}
+
+class TTWidget extends StatelessWidget {
+  late String tableNumber;
+  late String image;
+  late int enrolledSeats;
+  TTWidget({required this.image,
+    required this.enrolledSeats,required this.tableNumber});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 3,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GestureDetector(
+          onTap: () {
+            showModalBottomSheet<void>(
+                backgroundColor: Colors.lightBlueAccent,
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                        // bottomRight: Radius.circular(50.0),
+                        topRight: Radius.circular(50.0),
+                        topLeft: Radius.circular(56.0))),
+                context: context,
+                builder: (context) {
+                  return TableStatusPopup(
+                    tableno: 'Table NO $tableNumber',
+                    unEnrollSeats: enrolledSeats,
+                  );
+                });
+          },
+          child: Container(
+            padding: const EdgeInsets.only(right: 10),
+            // width: double.infinity,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(color: Colors.black,width: 5),
+              image: DecorationImage(
+                image: AssetImage(image),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Text(
+              "TABLE NO $tableNumber",
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
