890c389df3cf373928ed057fbf32e5abab8376af
Harshwardhan431 <81025973+Harshwardhan431@users.noreply.github.com>
2022-02-09 15:09:14 +0530

    final update

--- lib/screens/profile/profile_edit_srcreen.dart
+++ lib/screens/profile/profile_edit_srcreen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:psa/models/userDetails.dart';
-import 'package:psa/screens/profile/profile_screen.dart';
 
 class EditProfileScreen extends StatefulWidget {
   const EditProfileScreen({Key? key}) : super(key: key);
